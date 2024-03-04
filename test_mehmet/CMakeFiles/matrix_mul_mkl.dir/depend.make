1ad3dbecf75ace56a7c94d5e8ea952363f4a2263
maktukmak <mehmet.aktukmak@intel.com>
2024-03-04 19:52:55 +0000

    cmake torch fixed

--- test_mehmet/CMakeFiles/matrix_mul_mkl.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for matrix_mul_mkl.
-# This may be replaced when dependencies are built.
--- test_mehmet/CMakeFiles/test_torch_int8_mm_dnnl.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for test_torch_int8_mm_dnnl.
